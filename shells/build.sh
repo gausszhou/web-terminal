840120985a644fb0979d5148a116e90bbcf97467
gausszhou <gausszhou@qq.com>
2025-11-10 21:26:35 +0800

    fix: 优化构建脚本，调整目录切换顺序以确保正确构建所有模块

--- shells/build.sh
+++ shells/build.sh
@@ -5,10 +5,16 @@ pnpm i
 cd packages/web-terminal-common
 pnpm build
 
-cd ../web-terminal-portal
+cd -
+cd packages/web-terminal-mockpty
 pnpm build
 
-cd ../web-terminal-service
+cd -
+cd packages/web-terminal-portal
+pnpm build
+
+cd -
+cd packages/web-terminal-service
 pnpm build
 
 cd -
